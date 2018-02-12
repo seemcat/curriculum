4790163562976a8259cc05de9c0cd7cefad03323
mari <maricrisbonzo@gmail.com>
2018-02-11 16:35:41 -0800

    greatest common divisor

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -1,12 +1,15 @@
 /**
- * Greatest Common Denominator - returns greatest common denominator
+ * Greatest Common Divisor - returns greatest common divisor 
  * @param {number} a
  * @param {number} b
  * @returns {number}
  */
 
-const solution = (a, b) => {
-  return 0;
+const solution = (a, b, counter = 1, gcd) => {
+  if (a < counter || b < counter) return gcd;
+  if (a === b) return a;
+  if (a % counter === 0 && b % counter === 0) gcd = counter;
+  return solution(a, b, counter + 1, gcd);
 };
 
 module.exports = {
