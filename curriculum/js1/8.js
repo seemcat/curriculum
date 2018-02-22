eec9fac567b7a2d361e5d1a798eb6f99b1311a69
mari <maricrisbonzo@gmail.com>
2018-02-21 16:09:45 -0800

    removed unnecessary condition

--- curriculum/js1/8.js
+++ curriculum/js1/8.js
@@ -7,7 +7,6 @@
 
 const solution = (a, b, counter = 1, gcd) => {
   if (a < counter || b < counter) return gcd;
-  if (a === b) return a;
   if (a % counter === 0 && b % counter === 0) gcd = counter;
   return solution(a, b, counter + 1, gcd);
 };
